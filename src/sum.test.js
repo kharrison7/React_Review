cba015b85edb4ef5a687a63c67aa882442c9cf07
Kevin Harrison <kharrison7@elon.edu>
2019-03-17 11:57:00 -0400

    This allows the tests to work.

---
+++ src/sum.test.js
@@ -0,0 +1,9 @@
+//from https://jestjs.io/docs/en/getting-started
+
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  //one test should pass, the other should fail.
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(2, 2)).toBe(1);
+});
